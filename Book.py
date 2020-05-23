f1e6aa9a9c7edb8812b51f7055cb266e261dfc61
cpedroni <44426472+cpedroni@users.noreply.github.com>
2020-05-23 16:40:26 +0200

    Documenting

--- Book.py
+++ Book.py
@@ -3,6 +3,18 @@ from Character import Character
 
 @dataclass
 class Book():
+  """
+  Dataclass containing the Title and Author of the Book and its list of Characters
+  ...
+  Attributes
+  ----------
+  Title : str
+    Title of the Book
+  Author : str
+    Author of the Book
+  Characters : list
+    List of Character dataclasses of the Book
+  """
   Title:str
   Author:str
   Characters: list() = field(default="None")
